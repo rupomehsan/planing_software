b7fa18a303b0bf4277d409eda5cedcfd90c843fb
rupomehsan <rupomehsan34@gmail.com>
2025-01-12 15:53:40 +0600

    app config

--- resources/js/backend/Config/app_config.ts
+++ resources/js/backend/Config/app_config.ts
@@ -2,13 +2,15 @@ type app_config_type = {
     api_host: string;
     api_version: string;
     image_host: string;
-}
+};
 
-let app_config : app_config_type = {
+let app_config: app_config_type = {
     // api_host: location.origin,
-    api_host: "http://127.0.0.1:8000",
-    image_host: "http://127.0.0.1:8000",
-    api_version: 'api/v1'
-}
+    api_host: "https://planning.techparkit.org/",
+    image_host: "https://planning.techparkit.org/",
+    // api_host: location.origin + ":8000",
+    // image_host: location.origin + ":8000",
+    api_version: "api/v1",
+};
 
-export default app_config
+export default app_config;
