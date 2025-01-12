6544bf4641e4b6c60a7327daa998a777c03e6592
rupomehsan <rupomehsan34@gmail.com>
2025-01-12 16:00:37 +0600

    config udpate

--- resources/js/backend/Config/app_config.ts
+++ resources/js/backend/Config/app_config.ts
@@ -6,8 +6,8 @@ type app_config_type = {
 
 let app_config: app_config_type = {
     // api_host: location.origin,
-    api_host: "https://planning.techparkit.org/",
-    image_host: "https://planning.techparkit.org/",
+    api_host: "https://planning.techparkit.org",
+    image_host: "https://planning.techparkit.org",
     // api_host: location.origin + ":8000",
     // image_host: location.origin + ":8000",
     api_version: "api/v1",
